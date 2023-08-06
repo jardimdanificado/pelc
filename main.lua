c38074e6b325c7852857750454ab3fea88b9f170
jardimdanificado <jardeltorreslima01@gmail.com>
2023-08-06 00:24:27 -0300

    0.6.1 experimental bindings added

--- main.lua
+++ main.lua
@@ -17,28 +17,28 @@ api.file.save.text('changed.txt', api.encode.base64Decode(api.encode.load("abc.t
 local function teclado(session)
     if(rl.IsKeyDown(rl.KEY_PAGE_UP)) then
         session.scene.camera.position.y = session.scene.camera.position.y + 0.000000001
-        rl.UpdateCamera(session.scene.camera)
+        rl.UpdateCamera(session.scene.camera,0)
     elseif(rl.IsKeyDown(rl.KEY_PAGE_DOWN)) then
         session.scene.camera.position.y = session.scene.camera.position.y - 0.000000001
-        rl.UpdateCamera(session.scene.camera)
+        rl.UpdateCamera(session.scene.camera,0)
     elseif(rl.IsKeyDown(rl.KEY_UP)) then
         session.scene.camera.target.y = session.scene.camera.target.y + 0.000000001
-        rl.UpdateCamera(session.scene.camera)
+        rl.UpdateCamera(session.scene.camera,0)
     elseif(rl.IsKeyDown(rl.KEY_DOWN)) then
         session.scene.camera.target.y = session.scene.camera.target.y - 0.000000001
-        rl.UpdateCamera(session.scene.camera)
+        rl.UpdateCamera(session.scene.camera,0)
     elseif(rl.IsKeyDown(rl.KEY_RIGHT)) then
         session.scene.camera.position = api.math.rotate(session.scene.camera.position,session.scene.camera.target,-0.000000001)
-        rl.UpdateCamera(session.scene.camera)
+        rl.UpdateCamera(session.scene.camera,0)
     elseif(rl.IsKeyDown(rl.KEY_LEFT)) then
         session.scene.camera.position = api.math.rotate(session.scene.camera.position,session.scene.camera.target,0.000000001)
-        rl.UpdateCamera(session.scene.camera)
+        rl.UpdateCamera(session.scene.camera,0)
     elseif(rl.IsKeyDown(rl.KEY_PERIOD)) then
         session.scene.camera.fovy = session.scene.camera.fovy - 1
-        rl.UpdateCamera(session.scene.camera)
+        rl.UpdateCamera(session.scene.camera,0)
     elseif(rl.IsKeyDown(rl.KEY_COMMA)) then
         session.scene.camera.fovy = session.scene.camera.fovy + 1
-        rl.UpdateCamera(session.scene.camera)
+        rl.UpdateCamera(session.scene.camera,0)
     elseif rl.IsKeyPressed(rl.KEY_F1) then
         session.console.active = true
     end
